abd6cb1e742985a8f321b1d9b1dae17b168876a3
Leoneedus <marmaz.966@o2.pl>
2020-01-14 06:35:49 +0100

    Health Bars finally fixed

--- Source/DumberPlumber/HealthComponent.cpp
+++ Source/DumberPlumber/HealthComponent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "HealthComponent.h"
+#include "DumberPlumberCharacter.h"
 #include "Net/UnrealNetwork.h"
 
 
@@ -31,6 +32,16 @@ void UHealthComponent::BeginPlay()
 void UHealthComponent::HealthChanged()
 {
 	UE_LOG(LogTemp, Warning, TEXT("HealthChanged"));
+	auto plumber = (ADumberPlumberCharacter*)GetOwner();
+	if (plumber)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("plumber found"));
+		plumber->HealthInd();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("plumber not found"));
+	}
 }
 
 void UHealthComponent::HandleTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, 
